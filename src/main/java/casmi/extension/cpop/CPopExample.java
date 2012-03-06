3dadad360ebd07887cdad9c95b04cbda7ef50f85
Toshiki Takeuchi <take@xcoo.jp>
2012-03-06 14:22:53 +0900

    Add a license comment to CPopExample and refactoring it a little.

--- src/main/java/casmi/extension/cpop/CPopExample.java
+++ src/main/java/casmi/extension/cpop/CPopExample.java
@@ -1,3 +1,21 @@
+/*
+ *   casmi examples
+ *   http://casmi.github.com/
+ *   Copyright (C) 2011, Xcoo, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package casmi.extension.cpop;
 
 import casmi.Applet;
@@ -5,29 +23,34 @@ import casmi.AppletRunner;
 import casmi.KeyEvent;
 import casmi.MouseButton;
 import casmi.MouseEvent;
+import casmi.graphics.color.Color;
 import casmi.graphics.color.ColorSet;
 import casmi.graphics.color.RGBColor;
 
-public class CPopExample extends Applet{
+/**
+ * @author S. Yoshida
+ *
+ * @see casmi.extension.cpop.CPopupMenu
+ */
+public class CPopExample extends Applet {
 	
 	CPopupMenu menu;
-	RGBColor color;
-
-	RGBColor white;
+	
+	Color color;
+	Color white = new RGBColor(ColorSet.WHITE);;
 
 	@Override
 	public void setup() {
 		setSize(200, 200);
 
-		white = new RGBColor(ColorSet.WHITE);
 		color = white;
 
 		menu = new CPopupMenu(this);
 		menu.addMenuItem("clear", "clearColor");
 		menu.addSeparator();
-		menu.addMenuItem("red", "changeColor", new RGBColor(ColorSet.RED));
+		menu.addMenuItem("red",   "changeColor", new RGBColor(ColorSet.RED));
 		menu.addMenuItem("green", "changeColor", new RGBColor(ColorSet.GREEN));
-		menu.addMenuItem("blue", "changeColor", new RGBColor(ColorSet.BLUE));
+		menu.addMenuItem("blue",  "changeColor", new RGBColor(ColorSet.BLUE));
 	}
 
 	public void clearColor() {
@@ -58,7 +81,6 @@ public class CPopExample extends Applet{
 	}
 
 	public void main(String[] args) {
-		AppletRunner.run("casmi.extension.cpop.CPopExample",
-				"CPopExample");
+		AppletRunner.run("casmi.extension.cpop.CPopExample", "CPopExample");
 	}
 }
