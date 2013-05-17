93cce71633e9755248e4b0dcdf66fe1c6a1cdfad
Toshiki TAKEUCHI <take@xcoo.jp>
2013-05-17 11:47:42 +0900

    Refactor GlowExample

--- src/main/java/casmi/graphics/shader/GlowExample.java
+++ src/main/java/casmi/graphics/shader/GlowExample.java
@@ -1,8 +1,6 @@
-package casmi.graphics.shader;
-
 /*
  *   casmi examples
- *   http://casmi.github.com/
+ *   http://casmi.github.io/
  *   Copyright (C) 2011, Xcoo, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +16,8 @@ package casmi.graphics.shader;
  * limitations under the License.
  */
 
+package casmi.graphics.shader;
+
 import casmi.Applet;
 import casmi.AppletRunner;
 import casmi.KeyEvent;
@@ -32,103 +32,109 @@ import casmi.graphics.element.Text;
 import casmi.graphics.element.Texture;
 
 /**
- * GlowShading example
+ * GlowShading example.
  *
- * @see casmi.graphics.shader
+ * @author Y. Ban
  *
- * @author Y. BAN
+ * @see casmi.Applet#enableBlurShader()
+ * @see casmi.Applet#disableBlurShader()
+ * @see casmi.graphics.element.Element#enableBlur()
+ * @see casmi.graphics.element.Element#setBlurMode(BlurMode)
  */
-
 public class GlowExample extends Applet {
 
     static final String IMAGE_PATH = Applet.class.getResource("logo.png").getPath();
-    Rect r1,r2,r3;
-    Circle c;
-    MouseClickCallback mouseover;
-    Text t;
-    double radian = 0;
 
+    Rect r1, r2, r3;
+    Circle c;
+    Text t;
     Texture tex;
 
+    double radian = 0.0;
     double rot = 0.0;
 
     @Override
     public void setup() {
         setSize(800, 600);
+
         r1 = new Rect(100, 100);
-        r1.setPosition(100,100);
-        r2 = new Rect(100, 100);
-        r2.setPosition(150,150);
-        r3 = new Rect(100, 100);
-        r3.setPosition(200,200, 100);
-        t = new Text("c");
-        t.getFont().setSize(50);
-        t.setStrokeColor(ColorSet.LIGHT_BLUE);
-        t.setPosition(70, 70);
+        r1.setPosition(100, 100);
         r1.setFillColor(ColorSet.DARK_GREEN);
+
+        r2 = new Rect(100, 100);
+        r2.setPosition(150, 150);
         r2.setFillColor(ColorSet.ORANGE);
         r2.setStroke(false);
-        r3.enableBlur();
-        r3.setStroke(false);
-        r3.setFillColor(ColorSet.BLUE);
         r2.enableBlur();
         r2.setBlurMode(BlurMode.Glow);
+
+        r3 = new Rect(100, 100);
+        r3.setPosition(200, 200, 100);
+        r3.setFillColor(ColorSet.BLUE);
+        r3.setStroke(false);
+        r3.enableBlur();
+
+        t = new Text("c");
+        t.getFont().setSize(50);
+        t.setPosition(70, 70);
+        t.setStrokeColor(ColorSet.LIGHT_BLUE);
         t.enableBlur();
         t.setBlurMode(BlurMode.Glow);
+
         c = new Circle(30);
-        c.enableBlur();
-        c.setBlurMode(BlurMode.MotionBlur);
+        c.setPosition(500 + 100 * Math.cos((radian / 180.0) * Math.PI),
+            300 + 100 * Math.sin((radian / 180.0) * Math.PI));
         c.setFillColor(ColorSet.RED);
         c.setStrokeColor(ColorSet.RED);
-        c.setPosition(500 + 100 * Math.cos((radian/180.0) * Math.PI), 300 + 100 * Math.sin((radian/180.0) * Math.PI));
-        mouseover = new MouseClickCallback() {
+        c.enableBlur();
+        c.setBlurMode(BlurMode.MotionBlur);
+
+        MouseClickCallback mouseclick = new MouseClickCallback() {
 
             @Override
             public void run(MouseClickTypes eventtype, Element element) {
-
-                if(eventtype == MouseClickTypes.CLICKED) {
+                if (eventtype == MouseClickTypes.CLICKED) {
                     BlurMode blur = element.getBlurMode();
-                    if(element.isBlur()){
-                        if(blur==BlurMode.Blur)
+                    if (element.isBlur()) {
+                        if (blur == BlurMode.Blur)
                             element.setBlurMode(BlurMode.Glow);
-                        else if(blur==BlurMode.Glow)
+                        else if (blur == BlurMode.Glow)
                             element.disableBlur();
                     } else {
                         element.enableBlur();
                         element.setBlurMode(BlurMode.Blur);
                     }
                 }
-
             }
         };
-        r3.addMouseEventCallback(mouseover);
-        addObject(r1);
-        addObject(t);
-        addObject(r3);
-        addObject(r2);
-        addObject(c);
+        r3.addMouseEventCallback(mouseclick);
 
         tex = new Texture(IMAGE_PATH);
         tex.setPosition(500, 100);
         tex.setWidth(tex.getWidth() / 1.2);
         tex.setRotation(rot, 0.0, 1.0, 0.0);
-        addObject(tex);
         tex.enableBlur();
-        tex.addMouseEventCallback(mouseover);
+        tex.addMouseEventCallback(mouseclick);
+
         enableBlurShader();
 
-
+        addObject(r1);
+        addObject(t);
+        addObject(r3);
+        addObject(r2);
+        addObject(c);
+        addObject(tex);
     }
 
     @Override
     public void update() {
         rot += 2.0;
         tex.setRotation(rot, 0.0, 1.0, 0.0);
-        radian+=2;
-        if(radian > 360)
+        radian += 2;
+        if (radian > 360)
             radian = 0;
-        c.setPosition(500 + 100 * Math.cos((radian/180.0) * Math.PI), 300 + 100 * Math.sin((radian/180.0) * Math.PI));
-
+        c.setPosition(500 + 100 * Math.cos((radian / 180.0) * Math.PI),
+            300 + 100 * Math.sin((radian / 180.0) * Math.PI));
     }
 
     @Override
@@ -136,17 +142,13 @@ public class GlowExample extends Applet {
 
     @Override
     public void keyEvent(KeyEvent e) {
-        if(!isBlur()){
+        if (!isBlur())
             enableBlurShader();
-        }
-        else{
+        else
             disableBlurShader();
-        }
-
     }
 
     public static void main(String[] args) {
         AppletRunner.run("casmi.graphics.shader.GlowExample", "Glow Example");
     }
-
 }
